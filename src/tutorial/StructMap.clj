6e4db65825523a4dfabe132f09f33333a512afce
Russ Carroll <info@rcarroll.dev>
2021-09-27 22:54:14 -0400

    Completed lesson on StuctMaps.

---
+++ src/tutorial/StructMap.clj
@@ -0,0 +1,18 @@
+(ns tutorial.StructMap)
+
+(defn Pets
+  []
+  (defstruct pet :PetType :PetName)
+  (def myPet (struct pet "dog" "Fido" ))
+  (println myPet)
+
+  (def myOtherPet (struct-map pet :PetName "Fifi" :PetType "cat"))
+  (println myOtherPet)
+
+  (def myNewPet (assoc myPet :PetName "Max"))
+  (println myNewPet)
+
+  (def myNewOtherPet (assoc myOtherPet :PetAge 10))
+  (println myNewOtherPet)
+  )
+(Pets)
